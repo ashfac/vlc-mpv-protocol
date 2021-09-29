f3349cc371dee1743c2578c46140995db65092cd
Ashfaq Ahmad <ashfaq.ahmad@apnimail.duckdns.org>
2021-09-28 23:28:52 -0400

    download youtube video/audio from web browser

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo chmod +x vlc-mpv-protocol
 
 sudo cp vlc-mpv-protocol /usr/local/bin/
 
-xdg-desktop-menu install vlc-mpv-protocol.desktop
+xdg-desktop-menu install vlc-mpv-protocol.desktop youtube-dl.desktop
